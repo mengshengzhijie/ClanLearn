be6158e2b6b4b9aa394389cdd8e3cacc1d110058
MengShengZhiJie <mengshengzhijie@163.com>
2024-09-23 00:52:43 +0800

    2024/9/23第一次提交

--- src/learn/24_9_23-1.c
+++ src/learn/24_9_23-1.c
@@ -1,3 +1,32 @@
 //
 // Created by xuxiao on 24-9-23.
 //
+#include<stdio.h>
+
+// int main() {
+//     // char password[20] = {0};
+//     // printf("input\n");
+//     // scanf("%s",password);
+//     // // getchar();
+//     // int ch = 0;
+//     // while ((ch = getchar()) != '\n');
+//     // printf("Y/N\n");
+//     // int ret = getchar();
+//     // if (ret == 'y') {
+//     //     printf("YES");
+//     // } else {
+//     //     printf("NO");
+//     //     }
+//     char ch = '0';
+//     while ((ch = getchar()) != '') {
+//         if (ch < '0' || ch > '9')
+//                 continue;
+//             putchar(ch);
+//     }
+//
+// }
+
+int main() {
+
+}
+
