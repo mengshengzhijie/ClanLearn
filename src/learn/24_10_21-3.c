950d8454c9debbebaa626997bb334dae658a4357
MengShengZhiJie <mengshengzhijie@163.com>
2024-10-21 19:26:20 +0800

    2024/10/21第四次提交

--- src/learn/24_10_21-3.c
+++ src/learn/24_10_21-3.c
@@ -1,3 +1,50 @@
 //
 // Created by xuxiao on 24-10-21.
 //
+
+#include <stdio.h>
+// int main() {
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+//     int i = 14;
+//     int sz = sizeof(arr)/sizeof(arr[0]);
+//     // for (int j = 0; j <= sz; j++) {
+//     //     if (arr[j] == i) {
+//     //         printf("%d是下标",j);
+//     //     }
+//     // }
+//     // if (i > sz) {
+//     //     printf("没找到");
+//     // }
+//     int l = 0;
+//     int r = sz -1;
+//     int m = (l + r) / 2;
+//     if (i > arr[m]) {
+//         l = m + i;
+//     } else if (i < arr[m]) {
+//         r = m - i;
+//     } else {
+//         printf("%d是下标",m);
+//     }
+// }
+int main() {
+    int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int k = 15;
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int left = 0;
+    int right = sz - 1;
+
+   while (left <= right) {
+       int mid = (left + right) / 2;
+       if (arr[mid] < k) {
+           left = mid + 1;
+       } else if (arr[mid] > k) {
+           right = mid - 1;
+       } else {
+           printf("%d是下标",mid);
+           break;
+       }
+   }
+    if (left > right) {
+            printf("没找到");
+    }
+}
