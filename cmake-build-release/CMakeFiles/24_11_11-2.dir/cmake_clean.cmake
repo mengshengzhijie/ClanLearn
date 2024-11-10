d45862f4b1cb36e50f526ee838bdeb5f7ded5826
MengShengZhiJie <mengshengzhijie@163.com>
2024-11-11 04:33:47 +0800

    2024/11/11第二次提交

---
+++ cmake-build-release/CMakeFiles/24_11_11-2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "24_11_11-2"
+  "24_11_11-2.pdb"
+  "CMakeFiles/24_11_11-2.dir/src/learn/24_11_11-2.c.o"
+  "CMakeFiles/24_11_11-2.dir/src/learn/24_11_11-2.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/24_11_11-2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/learn/24_11_11-2.c
+++ src/learn/24_11_11-2.c
@@ -1,3 +1,54 @@
 //
 // Created by xuxiao on 24-11-11.
 //
+
+// #include <stdio.h>
+//
+// // void swap(int x,int y) {
+// //     int z = 0;
+// //     z = x;
+// //     x = y;
+// //     y = z;
+// // }
+// void swap2(int *x,int *y) {
+//     int z = 0;
+//     z = *x;
+//     *x = *y;
+//     *y = z;
+// }
+//
+// int add(int x, int y) {
+//     int z = 0;
+//     z = x + y;
+//     return z;
+// }
+// int main() {
+//     int a = 0;
+//     int b = 0;
+//     scanf("%d %d", &a, &b);
+//     printf("交换前 a = %d, b = %d\n",a,b);
+//     // swap(a, b);
+//     swap2(&a,&b);
+//     printf("交换后 a = %d, b = %d\n",a,b);
+//     add(a, b);
+//     printf("a + b = %d\n",add(a,b));
+// }
+
+// #include <stdio.h>
+// int main() {
+//     int i = 0;
+//     int flag = 1; //1表示是素数
+//     for (i = 100; i <= 200; i++) {
+//         int j = 0;
+//         for (j = 2; j <= i -1; j++) {
+//             if (i % j == 0) {
+//                 flag = 0;
+//                 break;
+//             }
+//         }
+//         if (flag == 1) {
+//             printf("%d\n", i);
+//         }
+//     }
+//
+// }
