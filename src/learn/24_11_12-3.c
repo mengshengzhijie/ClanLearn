c358293e8538b6e313c9541ef10ef84d3602946d
MengShengZhiJie <mengshengzhijie@163.com>
2024-11-12 23:04:00 +0800

    2024/11/12第七次提交

--- src/learn/24_11_12-3.c
+++ src/learn/24_11_12-3.c
@@ -2,6 +2,7 @@
 // Created by xuxiao on 24-11-12.
 //
 #include <stdio.h>
+#include <string.h>
 // int main(){
 //     unsigned int num = 0;
 //     scanf("%u", &num);
@@ -11,15 +12,32 @@
 //     }
 // }
 //递归的实现
-void print(unsigned int n) {
-    if (n > 9) {
-        print(n / 10);
-    }
-    printf("%d", n % 10);
-}
+// void print (unsigned int);
+// void print(unsigned int n) {
+//     if (n > 9) {
+//         print(n / 10);
+//     }
+//     //递归退出条件
+//     printf("%d", n % 10);
+// }
+//
+// int main() {
+//     unsigned int num = 1234;
+//     print(num);
+//     printf("\n\n");
+//     printf("%d",123 % 10);
+// }
 
+int my_strlen(char* str) {
+    int count = 0;
+    while (*str != '\0') {
+        count++;
+        str++;
+    }
+    return count;
+}
 int main() {
-    unsigned int num = 0;
-    scanf("%u", &num);
-    print(num);
+    char arr[] = {"abc"};
+    int len = my_strlen(arr);
+    printf("%d\n",len);
 }
