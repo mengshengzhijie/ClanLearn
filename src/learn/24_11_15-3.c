fb7e195a7188ebd079ac0c8771fb1b695b275a25
MengShengZhiJie <mengshengzhijie@163.com>
2024-11-15 06:52:34 +0800

    2024/11/15第三次提交

---
+++ src/learn/24_11_15-3.c
@@ -0,0 +1,44 @@
+//
+// Created by xuxiao on 24-11-15.
+//
+#include <stdio.h>
+
+// int main() {
+//     int arr[10] = {11, 2, 3, 4, 5, 6, 17, 8, 9, 10};
+//     // int j = 0;
+//     // for (int i = 0; i < 10; i++) {
+//     //     j = j + 1;
+//     //     if (arr[i] > arr[j]) {
+//     //         arr[j] = arr[i];
+//     //     }
+//     // }
+//     int max = arr[0];
+//     for (int i = 1; i < 10; i++) {
+//         if (arr[i] > max)
+//             max = arr[i];
+//     }
+//     printf("%d", max);
+// }
+//
+// int main() {
+//     for (int i = 1; i < 10; ++i) {
+//         for (int j = 1; j <= i; ++j) {
+//             // printf(" |%d X %d = %2d| ",j,i,i*j);
+//             printf(" |%d X %d = %-2d| ",j,i,i*j);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main () {
+//     int h = 0;
+//     for (int i = 9; i >= 1; --i) {
+//         h++;
+//         for (int j = 1; j <= i; j++ ) {
+//             printf(" |%d X %d = %2d| ",h,j,h*j);
+//
+//         }
+//         printf("\n");
+//     }
+// }
+
