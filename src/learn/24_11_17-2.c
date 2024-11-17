dea527ee622b7abc29064d85d6dbecd3944255a4
MengShengZhiJie <mengshengzhijie@163.com>
2024-11-17 16:44:38 +0800

    2024/11/17第二次提交

---
+++ src/learn/24_11_17-2.c
@@ -0,0 +1,61 @@
+//
+// Created by xuxiao on 24-11-17.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+// int main() {
+//     int arr1[] = {1,3,5,7,9};
+//     int arr2[] = {2,4,6,8,0};
+//     int sz = sizeof(arr1)/sizeof(arr1[0]);
+//     for(int i = 0; i < sz; i++) {
+//         int tmp = arr1[i];
+//         arr1[i] = arr2[i];
+//         arr2[i] = tmp;
+//     }
+//     for (int i = 0; i < sz; ++i) {
+//         printf("%d ", arr1[i]);
+//         printf("%d ", arr2[i]);
+//     }
+// }
+
+// void init(int arr[],int st) {
+//     for (int i = 0; i < st; ++i) {
+//         arr[i] = 0;
+//     }
+// }
+// void print(int arr[],int st) {
+//     for (int i = 0; i < st; ++i) {
+//         printf("%d ", arr[i]);
+//     }
+// }
+// void reverse(int arr[],int st) {
+//     // int tmp;
+//     // for (int i = 0; i < st/2; ++i) {
+//     //     tmp = arr[i];
+//     //     arr[i] = arr[st-i];
+//     //     arr[st-i] = tmp;
+//     // }
+//
+//
+// }
+// void arr_int(int arr[],int st) {
+//     for (int i = 0; i < st; ++i) {
+//         arr[i] = i;
+//     }
+// }
+// int main () {
+//     int arr[10] = {};
+//     int st = sizeof(arr) / sizeof(arr[0]) - 1;
+//     init(arr,st);
+//     print(arr,st);
+//     printf("\n");
+//     arr_int(arr,st);
+//     reverse(arr,st);
+//     print(arr,st);
+// }
+
+int main() {
+
+}
