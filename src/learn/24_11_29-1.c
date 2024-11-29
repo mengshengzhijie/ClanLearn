9affa744e850616e6e4adda089284ea7fa1b1a31
MengShengZhiJie <mengshengzhijie@163.com>
2024-11-29 17:23:05 +0800

    2024/11/29第二次提交

--- src/learn/24_11_29-1.c
+++ src/learn/24_11_29-1.c
@@ -4,6 +4,36 @@
 
 #include <stdio.h>
 
+// int main () {
+//     int a = 3;
+//     a = a + 5;
+//
+//     printf("a = %d\n", a);
+// }
+
+// int main () {
+//     int flag = 3;
+//     if (flag) {
+//
+//     }
+// }
+
+// int main () {
+//     int a = -3;
+//     printf("a = %d\n",~a);
+//
+// }
+
+// int main () {
+//     int a = 10;
+//     int* p = &a;
+//     *p = 20;
+//     printf("a = %d\n",*p);
+// }
+
 int main () {
+    int a =(int)3.14;
+    printf("a = %d\n",a);
+    printf("%lu",sizeof a);
 
 }
