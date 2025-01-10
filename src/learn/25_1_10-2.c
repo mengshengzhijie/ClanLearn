b6276b9e60679fb08bb99804e90e7d76a0c440b5
MengShengZhiJie <mengshengzhijie@163.com>
2025-01-10 13:51:19 +0800

    2025/1/10第二次提交

---
+++ src/learn/25_1_10-2.c
@@ -0,0 +1,54 @@
+//
+// Created by xuxiao on 25-1-10.
+//
+
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+
+// int main() {
+//     char ch;
+//     while (scanf("%c", &ch) != EOF) {
+//         scanf("%c", &ch);
+//         if (ch >= 'a' && ch <= 'z')
+//             printf("%c\n", toupper(ch));
+//         else if (ch >= 'A' && ch <= 'Z')
+//             printf("%c\n", tolower(ch));
+//         getchar();
+//     }
+// }
+//
+// int main () {
+//     char ch;
+//     scanf("%c", &ch);
+//     while (scanf("%c", &ch) != EOF) {
+//         // if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+//         if (isalpha(ch)){
+//
+//         }else {
+//
+//         }
+//     }
+// }
+
+// int main () {
+//     int max,score;
+//     for (int i = 0; i < 3; ++i) {
+//         scanf("%d", &score);
+//         if (score>max)
+//             max = score;
+//     }
+//     printf("%d", max);
+// }
+
+// int main() {
+//     for (int i = 10000, sum = 0; i < 100000; ++i) {
+//         for (int j = 1; j < 4; ++j) {
+//             int k = (int)pow(10, j);
+//             sum += (i % k) * (i / k);
+//         }
+//         if (sum == i) {
+//             printf("%d\n", i);
+//         }
+//     }
+// }
