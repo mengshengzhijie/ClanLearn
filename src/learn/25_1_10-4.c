9a47d19833888ea0537e04cef66745e254289a9d
MengShengZhiJie <mengshengzhijie@163.com>
2025-01-10 21:16:34 +0800

    2025/1/10第五次提交

--- src/learn/25_1_10-4.c
+++ src/learn/25_1_10-4.c
@@ -4,7 +4,56 @@
 
 #include <stdio.h>
 
+// int main() {
+//     int arr[10] = {0};
+//     printf("%d\n",&arr[9] - &arr[0]);
+// }
+
+// int main() { // 二级指针
+//         int a = 10;
+//         int* pa = &a; // pa是一个指针，指向a的地址
+//         int ** ppa = &pa;
+//         **ppa = 20;
+//         printf("a = %d\n",a);
+// }
+
+// int main() {
+//     int a = 10;
+//     int b = 20;
+//     int c = 30;
+//
+//     int arr[10];
+//
+//     int* pa = &a;
+//     int* pb = &b;
+//     int* pc = &c;
+//
+//     int* parr[10] = {&a,&b,&c};
+//     for (int i = 0; i < 3; ++i) {
+//         printf("%d\n",*(parr[i]));
+//     }
+//
+// }
+
 int main() {
-    int arr[10] = {0};
-    printf("%d\n",&arr[9] - &arr[0]);
+    // int arr[3][4] = {1,2,3,4,2,3,4,5,3,4,5,6};
+    // for (int i = 0; i < 3; ++i) {
+    //     for (int j = 0; j < 4; ++j) {
+    //         printf("%d ",arr[i][j]);
+    //     }
+    // }
+//     int arr1[4] = {1, 2, 3, 4};
+//     int arr2[4] = {2, 3, 4, 5};
+//     int arr3[4] = {3, 4, 5, 6};
+//
+//     int* parr[3] = {arr1, arr2, arr3};
+//     for (int i = 0; i < 3; ++i) {
+//         for (int j = 0; j < 4; ++j) {
+//             printf("%d ",*(parr[i] + j));
+//         }
+//         printf("\n");
+//     }
+
 }
+
+
