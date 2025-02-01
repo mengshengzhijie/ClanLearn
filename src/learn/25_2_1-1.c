57ab4dc5e8c17e66f55d2badc1651ca932b32442
MengShengZhiJie <mengshengzhijie@163.com>
2025-02-01 15:05:09 +0800

    2025/2/1第一次提交

---
+++ src/learn/25_2_1-1.c
@@ -0,0 +1,12 @@
+//
+// Created by xuxiao on 25-2-1.
+//
+
+#include <stdio.h>
+
+int main() {
+    char a = -1;
+    signed char b = -1;
+    unsigned char c = -1;
+    printf("a=%d,b=%d,c=%d",a,b,c);
+}
